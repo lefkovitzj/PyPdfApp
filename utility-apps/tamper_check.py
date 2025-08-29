7624da66ae90d0888c257ffa4423615dd2410cec
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2025-08-28 22:28:13 -0400

    Improve PEP8 compliance in tamper_check.py

--- utility-apps/tamper_check.py
+++ utility-apps/tamper_check.py
@@ -5,10 +5,13 @@
     File Name: tamper_check.py
 """
 
+# Standard library imports.
 import json
-import requests
 import hashlib
 import os
+
+# Third-party module imports.
+import requests
 
 def hash_md5(string_to_hash):
     """Create a MD5 hash of the string argument"""
@@ -20,28 +23,36 @@ def generate_all_py_file_hashes(hash_dir="/"):
     hashes = {}
     for dirfile in os.listdir(hash_dir):
         if dirfile.endswith(".py"):
-            with open(hash_dir + dirfile,"r") as pyfile:
+            with open(hash_dir + dirfile, "r", encoding="utf-8") as pyfile:
                 hashes[dirfile] = hash_md5(pyfile.read())
     return hashes
 
 def main():
     """Run the tamper checker for PyPdfApp"""
-    with open("../settings.json", "r") as json_settings:
+    with open("../settings.json", "r", encoding="utf-8") as json_settings:
         settings = json.load(json_settings)
-    settings_url = "https://raw.githubusercontent.com/lefkovitzj/PyPdfApp/main/settings.json" # Load from settings.json's data.
+    # Load from settings.json's data.
+    settings_url = "https://raw.githubusercontent.com/lefkovitzj/PyPdfApp/main/settings.json"
     github_url = "https://github.com/lefkovitzj/PyPdfApp/"
 
     tampered_files = []
 
     try:
-        url_data = requests.get(settings_url).json() # Get the data at "newest_version_settings_url" as JSON data.
+        # Get the data at "newest_version_settings_url" as JSON data.
+        url_data = requests.get(settings_url, timeout=10).json()
         source_hashes = url_data["source_file_hashes"]
-    except: # Request failed due to lack of internet connection or missing online file.
-        print(f"Please connect to wifi, if this issue persists, please submit a bug report at {github_url}.")
+    except Exception: # Request failed due to lack of internet connection or missing online file.
+        print(
+            ("Please connect to wifi, if this issue persists, "
+            f"please submit a bug report at {github_url}.")
+        )
         return
 
     if settings["version"] != url_data["version"]:
-        print(f"Update needed. Cannot check for file tampering. Download the most recent version of PyPdfApp from {github_url} and try again.")
+        print(
+            ("Update needed. Cannot check for file tampering. "
+             f"Download the most recent version of PyPdfApp from {github_url} and try again.")
+        )
         return
 
 
@@ -55,21 +66,37 @@ def main():
             for key in source_keys: # Check for missing files.
                 if key not in instance_keys:
                     tampered_files.append(f"File \"{key}\" is missing.")
-                elif source_hashes[key] != instance_hashes[key]: # Check for tampering with files that should exist.
+                elif source_hashes[key] != instance_hashes[key]:
                     tampered_files.append(f"File \"{key}\" has been modified.")
             for key in instance_keys: # Check for extra files.
                 if key not in source_keys:
                     tampered_files.append(f"File \"{key}\" is authorized.")
 
-            print(f"TAMPERING DETECTION WARNING!!!\nThe following files have been tampered with:\n{'\n'.join(tampered_files)}\n\nIt is very strongly recommended that you re-install the file(s) above from the original source code found at {github_url} before running PyPdfApp!")
+            print(
+                ("TAMPERING DETECTION WARNING!!!\n"
+                 "The following files have been tampered with:\n"
+                 f"{'\n'.join(tampered_files)}\n\n"
+                 "It is very strongly recommended that you re-install the file(s) above "
+                 f"from the original source code found at {github_url} before running PyPdfApp!")
+            )
             return
         else: # No additional or missing files were detected.
             for key in source_keys:
-                if source_hashes[key] != instance_hashes[key]: # Check for tampering with files that should exist.
+                if source_hashes[key] != instance_hashes[key]:
                     tampered_files.append(f"File \"{key}\" has been modified.")
-            print(f"TAMPERING DETECTION WARNING!!!\nThe following files have been tampered with:\n{'\n'.join(tampered_files)}\n\nIt is very strongly recommended that you re-install the file(s) above from the original source code found at {github_url} before running PyPdfApp!")
+            print(
+                ("TAMPERING DETECTION WARNING!!!\n"
+                 "The following files have been tampered with:\n"
+                 f"{'\n'.join(tampered_files)}\n\n"
+                 "It is very strongly recommended that you re-install the file(s) above "
+                 f"from the original source code found at {github_url} before running PyPdfApp!")
+            )
             return
-    print("TAMPERING DETECTION CHECK COMPLETE\nNo files have been tampered with, added, or removed.\nYour installation of PyPdfApp is up-to-date and matches the official source code files.")
+    print(
+        ("TAMPERING DETECTION CHECK COMPLETE\n"
+         "No files have been tampered with, added, or removed.\n"
+         "Your installation of PyPdfApp is up-to-date and matches the official source code files.")
+    )
 
 if __name__ == "__main__":
     main()

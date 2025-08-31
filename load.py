817de9e4d8e4a3d6146ec2df636df41b52cc9769
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2025-08-31 09:15:47 -0400

    Improve PEP8 compliance in load.py

--- load.py
+++ load.py
@@ -1,32 +1,23 @@
 """
     Author: lefkovitj (https://lefkovitzj.com)
-    File Last Modified: 8/28/2025
+    File Last Modified: 8/31/2025
     Project Name: PyPdfApp
     File Name: load.py
 """
 
 # Python Standard Library Imports.
-import os
 import sys
-from tkinter import *
-from tkinter import filedialog
 import traceback
 
 # Third-party Module Imports.
-from PIL import Image, ImageTk
 import fitz
 import customtkinter as ctk
 
-def gui_get_file(initial_directory="", limit_filetypes=[]):
-    """Open file explorer (using tkinter) to select a file"""
-    root = Tk() # Create the GUI window.
-    root.withdraw()
-    complete_file_path = filedialog.askopenfilename(title="File Select", initialdir = os.getcwd() + "/" + initial_directory, filetypes = limit_filetypes) # Select the file.
-    root.destroy()
-    file_path, file_name = os.path.split(complete_file_path) # Get the filepath and filename to return to the user.
-    return complete_file_path, file_name
+# Project imports.
+from manipulate import gui_get_file
 
 def open_pdf():
+    """Open and load a PDF via the system filedialog"""
     if len(sys.argv) == 1: # No argument provided, request file path.
         file_path = gui_get_file(limit_filetypes=[("PDF",".pdf")])[0]
         if file_path == "":
@@ -38,13 +29,14 @@ def open_pdf():
         fitz_doc = fitz.open(file_path)
         doc_password = ""
         while fitz_doc.is_encrypted:
-            if doc_password==None:
+            if doc_password is None:
                 return None
             pass_dialog = ctk.CTkInputDialog(text="Password", title="Open PDF")
             doc_password = pass_dialog.get_input()
             fitz_doc.authenticate(doc_password)
         return file_path, fitz_doc, doc_password
 
-    except Exception: # Handle any application errors by returning them to the user without crashing.
-            print(f"Error Message: \"{traceback.format_exc()}\"")
-            return None
+    # Handle any application errors by returning them to the user without crashing.
+    except Exception:
+        print(f"Error Message: \"{traceback.format_exc()}\"")
+        return None
